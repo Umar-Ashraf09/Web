e244285206bc9edc4da2427f58c2c72ae912276c
Umar-Ashraf09 <92431008+Umar-Ashraf09@users.noreply.github.com>
2022-12-05 01:16:43 +0530

    Add files via upload

---
+++ Character Limit/script.js
@@ -0,0 +1,20 @@
+
+let myText = document.getElementById("my-text");
+let result = document.getElementById("result");
+let limit = 60;
+result.textContent = 0 + "/" + limit;
+
+myText.addEventListener("input", function() {
+
+    let textLenght = myText.value.length;
+    result.textContent = textLenght + "/" + limit;
+
+    if(textLenght > limit) {
+        myText.style.borderColor = "#ff2851";
+        result.style.color = "#ff2851";
+    }
+    else {
+        myText.style.borderColor = "#b2b2b2";
+        result.style.color = "#737373";
+    }
+});
